6ca57ccdc144af70c42d81381bb7da16a7ad6ac4
Keith Williams <G00324844@gmit.ie>
2018-03-01 12:21:43 +0000

    Added a new project with a Runner class.

---
+++ playfair/src/ie/gmit/sw/ai/Runner.java
@@ -0,0 +1,15 @@
+package ie.gmit.sw.ai;
+
+/**
+ * This is a runner class to start the application.
+ */
+public class Runner {
+	
+	/**
+	 * This is the entrypoint to the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
